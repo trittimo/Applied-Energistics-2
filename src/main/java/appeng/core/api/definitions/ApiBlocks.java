812e8c59900b023bd4a34f23d070c2909638fd41
PrototypeTrousers <salomaoo@gmail.com>
2023-10-13 03:50:47 -0300

    fix #315

--- src/main/java/appeng/core/api/definitions/ApiBlocks.java
+++ src/main/java/appeng/core/api/definitions/ApiBlocks.java
@@ -561,6 +561,10 @@ public final class ApiBlocks implements IBlocks {
                 })
                 .build();
 
+        if (!stairs.maybeBlock().isPresent()) {
+            return new BlockDefinition(registryName, null, null);
+        }
+
         Verify.verify(stairs.maybeBlock().isPresent());
 
         return stairs;
